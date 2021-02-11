d78870a8c76a07a68e4ac171b12e2a607e3bff49
ForkΨKILLET <fork_killet@qq.com>
2021-02-11 13:33:03 +0800

    VER 210211
    -- plugin - javascript
    -- vimrc

---
+++ gohome.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/.vim
+ln -s ./vimrc ~/.vim/vimrc
+
+mkdir -p ~/.vim/bundle
+ln -s ./vimrc/javascript ~/.vim/bundle/javascript
+
+
---
+++ javascript/ftdetect/javascript.vim
@@ -0,0 +1,2 @@
+au BufNewFile,BufRead *.js set filetype=javascript
+
