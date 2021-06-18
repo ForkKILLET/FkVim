3c4d307aec56c244920f4e2c3de4ed410174679c
ForkΨKILLET <fork_killet@qq.com>
2021-06-18 21:12:17 +0800

    VER 210618
    !- gohome
       -- Vundle.

--- gohome.sh
+++ gohome.sh
@@ -1,9 +1,22 @@
-mkdir -p ~/.vim
-rm -f ~/.vim/vimrc
-ln -s ~/_/FkVim/vimrc ~/.vim/vimrc
+indent=$1
+E() {
+	for i in {0..$indent}; do echo -n "    "; done
+	echo "$1"
+}
 
+E "# Constructing directories"
+mkdir -p ~/.vim
 mkdir -p ~/.vim/bundle
 rm -rf ~/.vim/bundle/FkVim
 
-echo "Welcome home: Vim."
+E "# Linking dotfiles * .vim"
+rm -f ~/.vim/vimrc
+ln -s ~/_/FkVim/vimrc ~/.vim/vimrc
+
+E "# Installing Vundle?"
+read Q; if [[ Q = Y ]]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else E ^; fi
+
+E "# Welcoming Vim."
 
